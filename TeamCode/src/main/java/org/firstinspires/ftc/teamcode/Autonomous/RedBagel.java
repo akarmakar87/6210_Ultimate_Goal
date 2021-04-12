7f969447a36e24860fcfbbfbfcd1f409b63a7891
Lucas Jackson <lucas.c.jackson@gmail.com>
2021-04-12 10:13:46 -0500

    adjustments to ring pickup in Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedBagel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedBagel.java
@@ -14,10 +14,10 @@ public class RedBagel extends UltimateGoalLinearOpMode {
 
         initOpenCV();
 
-        waitForStart ();
+        waitForStart();
 
         int pos = detectStack();
-        pos = 4;
+        pos = 1;
         int angle1 = 0;
         int dist1 = 0;
 
@@ -35,7 +35,7 @@ public class RedBagel extends UltimateGoalLinearOpMode {
 
                 sleep(500);
 
-                driveAdjust(-1, 12 , 0, 5000);
+                driveAdjust(-1, 12, 0, 5000);
 
                 turnPID(90, 0.6 / 180, 0.00005, 0.1, 3000);
                 turnPID(180, 0.6 / 180, 0.00005, 0.1, 3000);
@@ -46,32 +46,32 @@ public class RedBagel extends UltimateGoalLinearOpMode {
                 break;
 
             case 1:
-
-                driveAdjust(1, 85, 0, 5000);
+                strafeAdjust(1, 2, 0, 3000);
+                driveAdjust(1, 87, 0, 5000);
 
                 setWobbleArm(true);
                 setWobbleClaw(false);
 
                 sleep(1000);
 
-                shooter.setPower(1);
+                shooter.setPower(.72);
 
-                driveAdjust(-1, 32, 0, 5000);
+                driveAdjust(-1, 35, 0, 5000);
 
-                strafeAdjust(1, 12, 0, 3000);
+                strafeAdjust(1, 11, 0, 3000);
 
                 turnPID(90, 0.6 / 180, 0.00005, 0.1, 3000);
                 turnPID(179, 0.6 / 180, 0.00005, 0.1, 3000);
 
 
-                angle1 = 190;
-                dist1 = 50;
+                angle1 = -20;
+                dist1 = 53;
                 break;
 
             case 4:
 
                 strafeAdjust(1, 12, 0, 3000);
-                driveAdjust(2, 108, -2,  5000);
+                driveAdjust(2, 108, -2, 5000);
 
                 shooter.setPower(.9);
 
@@ -86,7 +86,7 @@ public class RedBagel extends UltimateGoalLinearOpMode {
                 turnPID(179, 0.6 / 180, 0.00005, 0.1, 3000);
 
                 angle1 = 160;
-                dist1 = 66;
+                dist1 = 69;
                 break;
 
         }
@@ -107,19 +107,24 @@ public class RedBagel extends UltimateGoalLinearOpMode {
 
         if (pos == 0) {
             turnPID(129, 0.6 / 180, 0.00005, 0.1, 3000);
-        } else if (pos ==1) {
-            turnPID(130, 0.6 / 180, 0.00005, 0.1, 3000);
-        } else {
+        } else if (pos == 1) {
             intake.setPower(-1);
-            turnPID(100  , 0.6 / 180, 0.00005, 0.1, 3000);
+            turnPID(100, 0.6 / 180, 0.00005, 0.1, 3000);
             driveAdjust(1, 30, 0, 5000);
             turnPID(170, 0.6 / 180, 0.00005, 0.1, 3000);
             intake.setPower(0);
-
+        } else {
+            intake.setPower(-1);
+            turnPID(100, 0.6 / 180, 0.00005, 0.1, 3000);
+            driveAdjust(1, 30, 0, 5000);
+            turnPID(171, 0.6 / 180, 0.00005, 0.1, 3000);
+            intake.setPower(0);
         }
 
 
-        if (pos == 1 || pos == 0) {
+        if (pos == 1) {
+            driveAdjust(1, 12 , 0, 5000);
+        } else if (pos == 0) {
             driveAdjust(1, 32, 0, 5000);
         } else {
             driveAdjust(1, 15, 0, 5000);
@@ -132,50 +137,59 @@ public class RedBagel extends UltimateGoalLinearOpMode {
 
         turnPID(angle1, 0.6 / 180, 0.00005, 0.1, 3000);
 
-        if (pos == 1 || pos == 0) {
+        if (pos == 0) {
             driveAdjust(-1, dist1, 0, 5000);
-        } else {
+        } else if (pos == 4) {
             driveAdjust(-1, dist1, -3, 5000);
+        } else {
+            driveAdjust(1, dist1, -20, 5000);
         }
 
         if (pos == 0) {
-            turnPID(110,   0.6 / 180, 0.00005, 0.1, 3000);
+            turnPID(110, 0.6 / 180, 0.00005, 0.1, 3000);
             turnPID(-90, 0.6 / 180, 0.00005, 0.1, 3000);
         } else if (pos == 4) {
             turnPID(-40, 0.6 / 180, 0.00005, 0.1, 3000);
-        } else {
-            turnPID(-75, 0.6 / 180, 0.00005, 0.1, 3000);
-            driveAdjust(1, 2, 0, 5000);
         }
 
-        setWobbleArm(true);
+            setWobbleArm(true);
 
-        setWobbleClaw(false);
+            setWobbleClaw(false);
 
-        sleep(400);
-        setWobbleArm(false);
-        //wobbleArm.setTargetPosition(100);
+            sleep(400);
+            setWobbleArm(false);
+            //wobbleArm.setTargetPosition(100);
 
-        if (pos == 4) {
-            shooter.setPower(.8);
-            turnPID(173, 0.6 / 180, 0.00005, 0.1, 3000);
-            driveAdjust(1,  35, 180, 5000);
-            setLoader(true);
-            setLoader(false);
-            setLoader(true);
-            setLoader(false);
-            driveAdjust(-1,  5, 180, 5000);
-            shooter.setPower(0);
-        } else if (pos == 1) {
-            driveAdjust(-1, 5, 0, 5000);
-        } else if (pos == 0) {
-            driveAdjust(-1, 5, -90, 5000);
-            turnPID(5, 0.6 / 180, 0.00005, 0.1, 3000);
-            driveAdjust(1, 12, 0,    5000);
+            if (pos == 4) {
+                shooter.setPower(.8);
+                turnPID(160, 0.6 / 180, 0.00005, 0.1, 3000);
+                driveAdjust(1, 35, 180, 5000);
+                setLoader(true);
+                setLoader(false);
+                setLoader(true);
+                setLoader(false);
+                driveAdjust(-1, 7, 180, 5000);
+                shooter.setPower(0);
+            } else if (pos == 1) {
+                shooter.setPower(.575);
+                sleep(250);
+                turnPID(185, 0.6 / 180, 0.00005, 0.1, 3000);
+                driveAdjust(1, 18, 180, 5000);
+                sleep(300);
+                setLoader(true);
+                setLoader(false);
+                shooter.setPower(0);
+                driveAdjust(-1, 12, 180, 5000);
+            } else if (pos == 0) {
+                driveAdjust(-1, 5, -90, 5000);
+                turnPID(5, 0.6 / 180, 0.00005, 0.1, 3000);
+                driveAdjust(1, 12, 0, 5000);
+            }
+
         }
-
     }
 
-}
+
+
 
 
